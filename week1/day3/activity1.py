8cc1d4c52d8c0fba0e93e9f63b2f0bed10ecfbb5
TimRobertsKrab <troberts95@outlook.com>
2022-06-08 11:16:49 +0100

    adds day3/activity1.py

---
+++ week1/day3/activity1.py
@@ -0,0 +1,16 @@
+d = {"one" : 
+        {"species" : "dog", "colour": "black" },
+    "two" :
+        {"species" : "cat", "colour" : "ginger"},
+    "three" :
+        {"species" : "dragon", "colour": "green"}
+}
+
+print(d) 
+print("\n")
+print(d["two"])
+d["two"]["colour"] = "tabby"
+print(d["two"])
+
+for x in d["three"].values():
+    print(x)
