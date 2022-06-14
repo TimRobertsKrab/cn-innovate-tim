fd564c691f98ab4987fa4f6a49e02dbed4628f9b
TimRobertsKrab <troberts95@outlook.com>
2022-06-14 17:07:29 +0100

    tries to make a trail

--- week2/day2/activity/pong.py
+++ week2/day2/activity/pong.py
@@ -13,12 +13,17 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("PONG")
 
 ball = pygame.Rect(screen_width/2-15,screen_height/2-15,30,30)
+ball2 = pygame.Rect(screen_width/2-15,screen_height/2-15,30,30)
+ball3 = pygame.Rect(screen_width/2-15,screen_height/2-15,30,30)
+ball4 = pygame.Rect(screen_width/2-15,screen_height/2-15,30,30)
+ball5 = pygame.Rect(screen_width/2-15,screen_height/2-15,30,30)
 player = pygame.Rect(screen_width-20,screen_height/2-70,10,140)
 opponent = pygame.Rect(10,screen_height/2-70,10,140)
 bg_color = pygame.Color("grey12")
 light_grey = (200,200,200)
 
 ball_colour = light_grey
+red = pygame.Color("red")
 
 ball_speed_x = 7 * random.choice((1,-1))
 ball_speed_y = 7 * random.choice((1,-1))
@@ -30,9 +35,12 @@ player_speed_y_increment = 6
 
 opponent_speed = 7
 
-ball_coordinates = [[None]*2 for i in range(5)]
+ball_coordinates = [(0,0)]*20
 
 normalised_speed = 49.0/(98**0.5)
+
+font = pygame.font.SysFont('Arial',30)
+text = font.render("Hello",True,(255,255,255))
 
 
 def ball_animation():
@@ -49,7 +57,20 @@ def ball_animation():
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
+    
 
+    for i in range(1,len(ball_coordinates)):
+        ball_coordinates[i] = ball_coordinates[i-1]
+    ball_coordinates[0] = (ball.x,ball.y)
+
+    ball2.update(ball_coordinates[5][0],ball_coordinates[5][1],30,30)
+    ball3.update(ball_coordinates[8][0],ball_coordinates[8][1],30,30)
+    ball4.update(ball_coordinates[13][0],ball_coordinates[13][1],30,30)
+    ball5.update(ball_coordinates[19][0],ball_coordinates[19][1],30,30)
+
+    
+
+    
 def player_animation():
     player.y+=player_speed_y
     player.x+=player_speed_x
@@ -116,11 +137,17 @@ while True:
     opponent_animation()
     # Draw
     screen.fill(bg_color)
+    screen.blit(text,(screen_width/2,10))
     pygame.draw.rect(screen,light_grey,player)
     pygame.draw.rect(screen,light_grey,opponent)
+    
+    pygame.draw.ellipse(screen,red,ball2)
+    pygame.draw.ellipse(screen,red,ball3)
+    pygame.draw.ellipse(screen,red,ball4)
+    pygame.draw.ellipse(screen,red,ball5)
     pygame.draw.ellipse(screen,ball_colour,ball)
     pygame.draw.aaline(screen,light_grey,(screen_width/2,0),(screen_width/2,screen_height))
-
+    
     pygame.display.flip()
 
     clock.tick(60)
