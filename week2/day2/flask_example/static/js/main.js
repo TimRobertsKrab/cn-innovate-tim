0e206f7e16e9173c0f16b99bd19b70aa46bc61fc
TimRobertsKrab <troberts95@outlook.com>
2022-06-16 15:57:14 +0100

    adds javascript and POST functionality

--- week2/day2/flask_example/static/js/main.js
+++ week2/day2/flask_example/static/js/main.js
@@ -8,7 +8,13 @@ function taskHider(){
 }
 
 function darkMode(){
-    let element = document.getElementsByTagName("body");
+    let element = document.body
     element.classList.toggle("bg-danger")
+    element.classList.toggle("bg-dark")
 }
+
+function displayDate(){
+    document.getElementById("date").innerHTML = Date();
+}
+
 
